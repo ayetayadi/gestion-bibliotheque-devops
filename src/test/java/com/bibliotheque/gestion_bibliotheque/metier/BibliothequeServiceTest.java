4bcdf7c376419477ff2ec83566b6f997b9f23920
ayet ayadi <ayetayadi01@gmail.com>
2026-01-09 23:27:52 +0100

    test: test ci fail to see cd

--- src/test/java/com/bibliotheque/gestion_bibliotheque/metier/BibliothequeServiceTest.java
+++ src/test/java/com/bibliotheque/gestion_bibliotheque/metier/BibliothequeServiceTest.java
@@ -39,7 +39,7 @@ class BibliothequeServiceTest {
         long result = bibliothequeService.nombreBibliothequesActives();
 
         // THEN
-        assertEquals(5L, result);
+        assertEquals(6L, result);
     }
 
     @Test
